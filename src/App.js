e31a6fcefff4b2bc8c64dbd876376723e709b9d3
Chinthani <chinthani96@gmail.com>
2020-11-09 11:29:03 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Homepage from "./pages/Homepage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesPage from "./pages/ArticlesPage";
+import ArticleListPage from "./pages/ArticleListPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import NavBar from "./NavBar";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <NavBar/>
+                <div id="page-body"><Route path="/" component={Homepage} exact/>
+                    <Switch>
+                        <Route path="/about" component={AboutPage}/>
+                        <Route path="/article-list" component={ArticleListPage}/>
+                        <Route path="/article/:name" component={ArticlesPage}/>
+                        <Route component={NotFoundPage}/>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Homepage = () => {
+    return(
+        <React.Fragment>
+            <h1>Welcome to my blog!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aut beatae delectus dignissimos, distinctio et harum illo in magnam minus non praesentium repudiandae sapiente tempore tenetur totam velit? At, vero.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequuntur doloribus dolorum explicabo libero nulla, placeat possimus quidem sed soluta. Ad at culpa temporibus voluptates! Facere odio possimus qui sequi!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus excepturi fuga libero qui, quia sed voluptatum? Ad aspernatur consectetur deleniti est eum exercitationem iusto nam quaerat reiciendis, veniam voluptate voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eum laudantium magnam nemo reprehenderit sapiente tempore! Ab eius harum in magnam quasi tempora voluptatem! Eius eos fugiat illo iusto repellat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequuntur doloribus dolorum explicabo libero nulla, placeat possimus quidem sed soluta. Ad at culpa temporibus voluptates! Facere odio possimus qui sequi!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus excepturi fuga libero qui, quia sed voluptatum? Ad aspernatur consectetur deleniti est eum exercitationem iusto nam quaerat reiciendis, veniam voluptate voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eum laudantium magnam nemo reprehenderit sapiente tempore! Ab eius harum in magnam quasi tempora voluptatem! Eius eos fugiat illo iusto repellat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequuntur doloribus dolorum explicabo libero nulla, placeat possimus quidem sed soluta. Ad at culpa temporibus voluptates! Facere odio possimus qui sequi!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus excepturi fuga libero qui, quia sed voluptatum? Ad aspernatur consectetur deleniti est eum exercitationem iusto nam quaerat reiciendis, veniam voluptate voluptates. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum eum laudantium magnam nemo reprehenderit sapiente tempore! Ab eius harum in magnam quasi tempora voluptatem! Eius eos fugiat illo iusto repellat. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consequuntur doloribus dolorum explicabo libero nulla, placeat possimus quidem sed soluta. Ad at culpa temporibus voluptates! Facere odio possimus qui sequi!</p>
+        </React.Fragment>
+    )
+}
+
+export default Homepage
